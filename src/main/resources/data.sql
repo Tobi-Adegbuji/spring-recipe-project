c29c9298fc37a630b6285d2f67b590aa9372eb82
Tobi Adegbuji <Adegbuji_tobi@yahoo.com>
2020-06-05 13:04:48 -0400

    Initialized database with spring data source with initializer. Closes #6

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (category_name) VALUES ('American');
+INSERT INTO category (category_name) VALUES ('Italian');
+INSERT INTO category (category_name) VALUES ('Mexican');
+INSERT INTO category (category_name) VALUES ('Japanese');
+INSERT INTO unit_of_measure (unit_of_measure) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure (unit_of_measure) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (unit_of_measure) VALUES ('Cup');
+INSERT INTO unit_of_measure (unit_of_measure) VALUES ('Pinch');
+INSERT INTO unit_of_measure (unit_of_measure) VALUES ('Ounce');
