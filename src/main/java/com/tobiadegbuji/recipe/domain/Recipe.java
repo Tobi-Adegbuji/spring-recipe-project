5e02775d6d8af5669848581fb68b574ac572ddc1
Tobi Adegbuji <Adegbuji_tobi@yahoo.com>
2020-06-04 23:05:28 -0400

    Created One to Many Relationship between Recipe and Ingredient (Bi-Directional)

--- src/main/java/com/tobiadegbuji/recipe/domain/Recipe.java
+++ src/main/java/com/tobiadegbuji/recipe/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.tobiadegbuji.recipe.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -25,6 +26,9 @@ public class Recipe {
 
     //todo add
     //private Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob //Will be created as a BLOB field in the database
     private Byte[] image;
@@ -94,6 +98,14 @@ public class Recipe {
 
     public void setDirections(String directions) {
         this.directions = directions;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 
     public Byte[] getImage() {
