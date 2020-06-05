f7dc73cedc17b630a4bf8ff835ea08993288cff1
Tobi Adegbuji <Adegbuji_tobi@yahoo.com>
2020-06-04 22:45:48 -0400

    Created one to one relationship for Recipe and Notes

---
+++ src/main/java/com/tobiadegbuji/recipe/domain/Notes.java
@@ -0,0 +1,41 @@
+package com.tobiadegbuji.recipe.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob //Will be stored in CLOB field in database. This allows users to write more than 256 characters for the notes.
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
