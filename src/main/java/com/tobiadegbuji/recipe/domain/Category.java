2462e91222878aef6c9802ec6c832d3e80eaec98
Tobi Adegbuji <Adegbuji_tobi@yahoo.com>
2020-06-05 09:49:30 -0400

    Created many to many relationship. Closes #3

---
+++ src/main/java/com/tobiadegbuji/recipe/domain/Category.java
@@ -0,0 +1,41 @@
+package com.tobiadegbuji.recipe.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String categoryName;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
