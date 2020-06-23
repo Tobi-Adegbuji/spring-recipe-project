634c215a07061b50adccc7afe5ad674101711f1a
Tobi Adegbuji <Adegbuji_tobi@yahoo.com>
2020-06-23 16:42:18 -0400

    Added test unit test for category id

---
+++ src/test/java/com/tobiadegbuji/recipe/domain/CategoryTest.java
@@ -0,0 +1,33 @@
+package com.tobiadegbuji.recipe.domain;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryTest {
+
+    Category category;
+
+    @BeforeEach
+    public void setUp(){
+        category = new Category();
+    }
+
+    @Test
+    void getId() {
+        Long idValue = 4L;
+        category.setId(idValue);
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    void getCategoryName() {
+    }
+
+    @Test
+    void getRecipes() {
+    }
+}
