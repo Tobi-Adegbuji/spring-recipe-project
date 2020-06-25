9441d943889c54145ac241ea9b186489e5dbc4a7
Tobi Adegbuji <Adegbuji_tobi@yahoo.com>
2020-06-24 23:49:51 -0400

    Spring integration test added to Uom.

---
+++ src/test/java/com/tobiadegbuji/recipe/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,38 @@
+package com.tobiadegbuji.recipe.repositories;
+
+import com.tobiadegbuji.recipe.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    @DirtiesContext //Reloads context
+    void findByUnitOfMeasureEach() throws Exception{
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByUnitOfMeasure("each");
+        assertEquals("each",uomOptional.get().getUnitOfMeasure());
+    }
+
+    void findByUnitOfMeasureCup() throws Exception{
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByUnitOfMeasure("cup");
+        assertEquals("cup",uomOptional.get().getUnitOfMeasure());
+    }
+}
