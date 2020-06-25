991756a63cf3ac43cfb58b2cd14b311adc5b5cc1
Tobi Adegbuji <Adegbuji_tobi@yahoo.com>
2020-06-24 22:39:39 -0400

    Spring test MockMvc added.

--- src/test/java/com/tobiadegbuji/recipe/controllers/IndexControllerTest.java
+++ src/test/java/com/tobiadegbuji/recipe/controllers/IndexControllerTest.java
@@ -9,6 +9,11 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -17,6 +22,7 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 class IndexControllerTest {
 
@@ -32,6 +38,15 @@ class IndexControllerTest {
     void setUp() {
         MockitoAnnotations.initMocks(this);
         indexController = new IndexController(recipeService);
+    }
+
+
+    @Test
+    public void testMockMVC() throws Exception {
+        //Stand alone doesnt bring up spring context.  Light Weight.
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(view().name("index"));
     }
 
     @Test
@@ -61,3 +76,11 @@ class IndexControllerTest {
         //any[Type]() : mocks the behavior of set type. Other Ex: anyString, anyInt and more
     }
 }
+
+
+
+//    ArgumentCaptor should be used whenever the built in argument matchers (e.g. equal) aren't enough for what you're trying to assert.
+//        This typically happens when you're verifying a method with one or more complex parameter types (e.g. user defined/third-party library
+//        object). Imagine you have this method that takes a third-party class that does not define 'equals/hasCode'.
+//        To verify this method you'll need an ArgumentCaptor (or custom matcher) since the built-in 'eq' matcher won't work since it
+//        requires that the parameter class implements 'equals'.
